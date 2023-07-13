dac295599d02454e223126f6fe7fd468a2450587
Pablo Cortez <cspablocortez@gmail.com>
2023-07-12 17:02:48 -0700

    Access token script

---
+++ get_access_token.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# GET THESE VALUES FROM SPOTIFY
+CLIENT_ID=""
+CLIENT_SECRET=""
+
+access_refresh=$(curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET")
+
+access_token=$(echo "$access_refresh" | jq -r '.access_token')
+
+echo "$access_token" > "./access_token"
+
