b7050326c9243ced9770d82c37865dcfc1002c20
Pablo Cortez <cspablocortez@gmail.com>
2023-07-14 17:43:21 -0700

    Testing scripts

--- get_access_token.sh
+++ get_access_token.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# GET THESE VALUES FROM SPOTIFY
-CLIENT_ID=""
-CLIENT_SECRET=""
+# GET THESE VALUES FROM SPOTIFY DEVELOPER DASHBOARD
+CLIENT_ID="09d8435e360d438aad8f71c48044d7fe"
+CLIENT_SECRET="a983af0557044085a141e7f5a44ac723"
 
 access_refresh=$(curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
@@ -10,5 +10,4 @@ access_refresh=$(curl -X POST "https://accounts.spotify.com/api/token" \
 
 access_token=$(echo "$access_refresh" | jq -r '.access_token')
 
-echo "$access_token" > "./access_token"
-
+echo -n "$access_token" > "./access_token"
