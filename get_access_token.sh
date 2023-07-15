681264faef689367b83b5122eb947c751bcfc298
Pablo Cortez <cspablocortez@gmail.com>
2023-07-14 17:44:35 -0700

    Remove access token and client ID

--- get_access_token.sh
+++ get_access_token.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # GET THESE VALUES FROM SPOTIFY DEVELOPER DASHBOARD
-CLIENT_ID="09d8435e360d438aad8f71c48044d7fe"
-CLIENT_SECRET="a983af0557044085a141e7f5a44ac723"
+CLIENT_ID=""
+CLIENT_SECRET=""
 
 access_refresh=$(curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
